561c8e07d69273b5b6f2f310cdab5c434a1ae597
Enrico Lumetti <e.lumetti@studenti.unipi.it>
2024-07-18 22:25:00 +0200

    Add button handling again

--- main2.py
+++ main2.py
@@ -21,6 +21,7 @@ class CanMessageFactory:
             "EXTERNAL_SimulatorInput"
         )
 
+
     def generate_simulator_input(self, pps: float, brake_force: float, angle: float) -> can.Message:
         data = self.simulator_input_msg.encode(
             {
@@ -65,12 +66,13 @@ class Joystick:
         self._trigger_right = 0
         self._left_x = 0
         self._left_y = 0
+        self._button_events = []
+        self._button_states = [False] * 6
 
     def connect(self):
         event = SDL_Event()
         while SDL_PollEvent(ctypes.byref(event)) != 0:
             if event.type == SDL_CONTROLLERDEVICEADDED:
-                print(dir(event.cdevice))
                 self.device = SDL_GameControllerOpen(event.cdevice.which)
                 self.guid = SDL_JoystickGetDeviceGUID(event.cdevice.which)
                 self.jdevice = event.jdevice
@@ -90,6 +92,10 @@ class Joystick:
                     self._left_x = 2*((value + 2**15) / (2**16-1) - 0.5)
                 elif event.caxis.axis == SDL_CONTROLLER_AXIS_LEFTY:
                     self._left_y = 2 *((value + 2**15) / (2**16-1) - 0.5)
+            elif event.type == SDL_CONTROLLERBUTTONDOWN or event.type == SDL_CONTROLLERBUTTONUP:
+                value = SDL_GameControllerGetButton(self.device, event.cbutton.button)
+                self._button_events.append((event.cbutton.button, value,))
+
 
     def trigger_left(self) -> float:
         return self._trigger_left
@@ -100,21 +106,44 @@ class Joystick:
     def axis_left(self) -> tuple[float]:
         return (self._left_x, self._left_y)
 
+    def get_button_event(self) -> can.Message | None:
+        if len(self._button_events) > 0:
+            button_num, state = self._button_events[0]
+
+            if button_num == SDL_CONTROLLER_BUTTON_A:
+                self._button_states[0] = bool(state)
+            elif button_num == SDL_CONTROLLER_BUTTON_B:
+                self._button_states[1] = bool(state)
+            elif button_num == SDL_CONTROLLER_BUTTON_X:
+                self._button_states[6] = bool(state)
+
+            self._button_events = self._button_events[1:]
+
+            return MSG_FACTORY.generate_sw_button(self._button_states)
+
+        return None
+
 
 if __name__ == "__main__":
     joystick = Joystick()
     with can.Bus("vcanv", interface="socketcan") as canv:
-        while True:
-            if joystick.connect():
-                break
-            time.sleep(0.1)
+        with can.Bus("vcans", interface="socketcan") as cans:
+            while True:
+                if joystick.connect():
+                    print("Controller detected")
+                    break
+                time.sleep(0.1)
 
-        while True:
-            joystick.update()
-            pps = joystick.trigger_left()
-            brake_pressure = joystick.trigger_right() * 400
-            delta_v = joystick.axis_left()[0] * math.pi
-            sim_input = MSG_FACTORY.generate_simulator_input(pps, brake_pressure, delta_v)
-            canv.send(sim_input)
-            time.sleep(0.1)
+            while True:
+                joystick.update()
+                pps = joystick.trigger_left()
+                brake_pressure = joystick.trigger_right() * 400
+                delta_v = joystick.axis_left()[0] * math.pi
+                sim_input = MSG_FACTORY.generate_simulator_input(pps, brake_pressure, delta_v)
+                canv.send(sim_input)
+                button_msg = joystick.get_button_event()
+                if button_msg is not None:
+                    cans.send(button_msg)
+
+                time.sleep(0.1)
 
